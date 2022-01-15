c5d20dc2dc977fd17a4a6be722a01700841d1b54
Krishna Rowter <krishna.rowter@gmail.com>
2022-01-15 20:51:15 +0700

    add navbar component and story

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,1 +1,48 @@
-export const Navbar = () => <nav></nav>
+import styles from './navbar.module.css'
+
+const PlaceholderIcon = () => <img src="https://via.placeholder.com/24x24" />
+
+export const Navbar = () => {
+    return (
+        <div className={styles.navbar}>
+            <a href="/">Milligram</a>
+
+            <form role="search">
+                <input type="search" placeholder="Search" />
+            </form>
+
+            <nav className={styles.navitems}>
+                <li>
+                    <a href="/">
+                        <PlaceholderIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="/direct/inbox">
+                        <PlaceholderIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="/create/select">
+                        <PlaceholderIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="/explore">
+                        <PlaceholderIcon />
+                    </a>
+                </li>
+                <li>
+                    <button>
+                        <PlaceholderIcon />
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <PlaceholderIcon />
+                    </button>
+                </li>
+            </nav>
+        </div>
+    )
+}
