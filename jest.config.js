11e0b2edd422fa55675b35aab14b47685697b43f
Krishna Rowter <krishna.rowter@gmail.com>
2022-01-15 14:01:13 +0700

    setup testing environment

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+  verbose: true,
+  testEnvironment: "jsdom",
+  moduleDirectories: ["./node_modules", "src"],
+  setupFilesAfterEnv: ["<rootDir>/src/jest-setup.ts"],
+};
+
+module.exports = config;
