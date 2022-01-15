4fdb6a2a9d264376fb2761549cacc1e66febb5b3
Krishna Rowter <krishna.rowter@gmail.com>
2022-01-15 14:20:36 +0700

    add navbar test and component

---
+++ src/Layout/Layout.test.tsx
@@ -0,0 +1,27 @@
+import { screen, render } from "@testing-library/react";
+
+import { Layout } from "./Layout";
+
+jest.mock("../Navbar/Navbar", () => ({
+  Navbar: () => <nav data-testid="navbar"></nav>,
+}));
+
+describe("<Layout />", () => {
+  it("should have Navbar", () => {
+    render(<Layout />);
+
+    expect(screen.getByTestId("navbar"));
+  });
+
+  it("should render its children wrapper in a div", () => {
+    render(
+      <Layout>
+        <div>any children</div>
+        <div>any children</div>
+        <div>any children</div>
+      </Layout>
+    );
+
+    expect(screen.getAllByText("any children")).toHaveLength(3)
+  });
+});
--- src/jest-setup.ts
+++ src/jest-setup.ts
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/react";
 import React from "react";
 
