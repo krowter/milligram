c7011d9f33439107d4b82729587dcf1960ec2128
Krishna Rowter <krishna.rowter@gmail.com>
2022-01-30 16:13:45 +0700

    add import aliases to babel and tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: [],
-};
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+    ],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                alias: {
+                    assets: './src/assets',
+                    components: './src/components',
+                    utils: './src/utils',
+                },
+            },
+        ],
+    ],
+}
